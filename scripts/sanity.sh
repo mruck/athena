d6e0cbe268b71f38d5864103867047d111ba0da0
marlies ruck <marlies.ruck@gmail.com>
2019-03-07 09:29:55 -0800

    cleaned up sanity

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -8,12 +8,12 @@ branch=$(git branch | grep \* | cut -d ' ' -f 2)
 ref=$(git rev-parse $branch | tr -d '\n')
 echo "Git branch/ref: $branch/$ref"
 GIT_SHA=$(git log | head -n 1 | cut -f 2 -d ' ' | head -c 10)
-#[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ] && GIT_SHA=$GIT_SHA-$RANDOM
+[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ] && GIT_SHA=$GIT_SHA-$RANDOM
 POD_NAME=$GIT_SHA-$RANDOM
 
 # Build img and tag with git sha
-#make fuzzer-client
-# docker push gcr.io/athena-fuzzer/athena:$GIT_SHA
+make fuzzer-client
+docker push gcr.io/athena-fuzzer/athena:$GIT_SHA
 
 mkdir -p /tmp/sanity/$POD_NAME
 
@@ -23,21 +23,18 @@ jq '.spec.containers[2].image = "gcr.io/athena-fuzzer/athena:'$GIT_SHA'"' pods/s
     jq '.metadata.name = "'$POD_NAME'"' > /tmp/sanity/$POD_NAME/pod.json 
 kubectl apply -f /tmp/sanity/$POD_NAME/pod.json 
 
-echo "Tail logs of client at /tmp/sanity/client"
+echo "Tail logs of client at /tmp/sanity/$POD_NAME/client"
+sleep 30
 (kubectl logs -f $POD_NAME athena  2>&1) > /tmp/sanity/$POD_NAME/client
 
-# Delete pod
 #kubectl delete pod $POD_NAME
 
-#output="misc/sanity.txt"
 #if [ -z "$1" ]; then
 #    (./orchestrate.py repro --client --stop_after_har $port 2>&1) > /tmp/sanity/$port/client
 #elif [ "$1" = "all" ]; then
 #    (./orchestrate.py repro --client --stop_after_all_routes $port 2>&1) > /tmp/sanity/$port/client
-#    output="misc/all_routes.txt"
 #fi
 #
-## Delete the pod
 #
 
 # Parse the client logs for run info. Info should look like this:
@@ -46,9 +43,10 @@ echo "Tail logs of client at /tmp/sanity/client"
 # Success Ratio: 0.5667752442996743
 # Total requests: 307
 cnt=$(cat /tmp/sanity/$POD_NAME/client | grep "Code Counts" | cut -d ':' -f 2- | tr -d ' ' | tr -d '\r')
-cov=$(cat /tmp/sanity$POD_NAME/client | grep "Final Coverage" | cut -d ':' -f 2 | tr -d ' ' | tr -d '\r')
+cov=$(cat /tmp/sanity/$POD_NAME/client | grep "Final Coverage" | cut -d ':' -f 2 | tr -d ' ' | tr -d '\r')
 succ=$(cat /tmp/sanity/$POD_NAME/client | grep "Success Ratio" | cut -d ':' -f 2 | tr -d ' ' | tr -d '\r')
 reqs=$(cat /tmp/sanity/$POD_NAME/client | grep "Total requests" | cut -d ':' -f 2 | tr -d ' ' | tr -d '\r')
+output="misc/sanity.txt"
 
 mkdir -p misc
 echo "Code counts: $cnt"
