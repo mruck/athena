849288a789959224ff42a7a85e1b8d3bbb2c9708
marlies ruck <marlies.ruck@gmail.com>
2019-03-12 21:44:34 -0700

    Keep track of unique exns in memory
    But currently broken so also git tracked debug code

--- lib/exception.py
+++ lib/exception.py
@@ -9,16 +9,64 @@ BENIGN_EXCEPTIONS = [
 ]
 
 
+class Exception(object):
+    # TODO: Add backtrace!!!!
+    def __init__(self, verb, path, cls, message):
+        self.verb = verb
+        self.path = path
+        self.cls = cls
+        self.message = message
+
+    @classmethod
+    def from_file(cls, exn_dict, route):
+        # TODO: pull out more stuff from rpoute, like params
+        cls(route.verb, route.path, exn_dict["class"], exn_dict["msg"])
+
+
 # Keep track of unique exceptions as well as pointer to exceptions log dumped
 # by rails
 class ExceptionTracker(object):
     def __init__(self, exceptions_file):
         self.exceptions_file_pointer = util.open_wrapper(exceptions_file, "r")
 
+    def merge(self, new_exns):
+        delta_exns = []
+        for new_exn in new_exns:
+            found = False
+            for unique_exn in self.unique_exceptions:
+                if (
+                    new_exn.verb == unique_exn.verb
+                    and new_exn.path == unique_exn.path
+                    and new_exn.cls == unique_exn.cls
+                ):
+                    found = True
+                    break
+            if not found:
+                self.unique_exns.append(new_exn)
+                delta_exns.append(new_exn)
+                with open("/tmp/exn", "a") as f:
+                    f.write("%s %s\n" % (new_exn.path, new_exn.cls))
+        return delta_exns
+
     # Read exceptions from the exception log and update the list of unique
     # exceptions
-    def update(self):
+    def update(self, route):
         exns = [json.loads(line.strip()) for line in self.exceptions_file_pointer]
         # Filter out benign exceptions
         malign_exns = [e for e in exns if e["class"] not in BENIGN_EXCEPTIONS]
-        return malign_exns
+
+        # Instantiate Exception objs
+        exn_objs = [Exception.from_dict(e, route) for e in malign_exns]
+        with open("/tmp/exn", "a") as f:
+            f.write("unfiltered exns:\n")
+            for e in exns:
+                f.write("%s %s\n" % (e["class"]))
+            f.write("malign exns:\n")
+            for e in malign_exns:
+                f.write("%s %s\n" % (e["class"]))
+            f.write("malign exns objs:\n")
+            for e in exn_objs:
+                f.write("%s %s\n" % (e.path, e.cls))
+        # Merge with the unique exceptions
+        delta_exns = self.merge(exn_objs)
+        return []
