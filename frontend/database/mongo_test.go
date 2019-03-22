5aae90ca1f7aefbafe06273c18d9e7f63e2b8b4c
marlies ruck <marlies.ruck@gmail.com>
2019-03-21 22:31:26 -0700

    basic lookup is working

--- frontend/database/mongo_test.go
+++ frontend/database/mongo_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -22,37 +23,47 @@ func (ex Exception) ToBSON() bson.M {
 	}
 }
 
-func TestMongoClient(t *testing.T) {
-	t.Run("connect", func(t *testing.T) {
-		cli, err := NewClient("localhost", "27017", "athena")
-		require.NoError(t, err)
-		require.NotNil(t, cli)
-	})
+func TestWriteRead(t *testing.T) {
+	cli, err := NewClient("localhost", "27017", "athena")
+	require.NoError(t, err)
+	require.NotNil(t, cli)
 
-	//	t.Run("put then get", func(t *testing.T) {
-	//		cli, err := NewClient("localhost", "27017", "athena")
-	//		require.NoError(t, err)
-	//		require.NotNil(t, cli)
-
-	//		exc := exception{Verb: "v", Path: "p"}
-	//		err = cli.WriteOne("exceptions", exc)
-	//		require.NoError(t, err)
-	//
-	//		result := exception{}
-	//		err = cli.ReadOne("exceptions", bson.M{"Verb": "v", "Path": "p"}, &result)
-	//		require.NoError(t, err)
-	//		require.Equal(t, "v", result.Verb)
-	//		require.Equal(t, "p", result.Path)
-	//	})
-	//	// read exception inserted from python
-	//	t.Run("read exception", func(t *testing.T) {
-	//		cli, err := NewClient("localhost", "27017", "athena")
-	//		require.NoError(t, err)
-	//		require.NotNil(t, cli)
-	//		result := exception{}
-	//		err = cli.ReadOne("exceptions", bson.M{"target_id": "2bedb7f5501f4b4393b479e3a5e91bf6"}, &result)
-	//		require.NoError(t, err)
-	//		require.Equal(t, result.Verb, "put")
-	//		require.Equal(t, result.Path, "/this/is/a/test/route")
-	//	})
+	cli.WriteOne("exceptions", &Exception{"v", "p", "c", "m", "t"})
+	var result Exception
+	cli.ReadOne("exceptions", bson.M{"verb": "v"}, &result)
+	fmt.Println(result.Verb)
+	fmt.Println(result.Path)
 }
+
+func TestMongoClient(t *testing.T) {
+	cli, err := NewClient("localhost", "27017", "athena")
+	require.NoError(t, err)
+	require.NotNil(t, cli)
+}
+
+//	t.Run("put then get", func(t *testing.T) {
+//		cli, err := NewClient("localhost", "27017", "athena")
+//		require.NoError(t, err)
+//		require.NotNil(t, cli)
+
+//		exc := exception{Verb: "v", Path: "p"}
+//		err = cli.WriteOne("exceptions", exc)
+//		require.NoError(t, err)
+//
+//		result := exception{}
+//		err = cli.ReadOne("exceptions", bson.M{"Verb": "v", "Path": "p"}, &result)
+//		require.NoError(t, err)
+//		require.Equal(t, "v", result.Verb)
+//		require.Equal(t, "p", result.Path)
+//	})
+//	// read exception inserted from python
+//	t.Run("read exception", func(t *testing.T) {
+//		cli, err := NewClient("localhost", "27017", "athena")
+//		require.NoError(t, err)
+//		require.NotNil(t, cli)
+//		result := exception{}
+//		err = cli.ReadOne("exceptions", bson.M{"target_id": "2bedb7f5501f4b4393b479e3a5e91bf6"}, &result)
+//		require.NoError(t, err)
+//		require.Equal(t, result.Verb, "put")
+//		require.Equal(t, result.Path, "/this/is/a/test/route")
+//	})
