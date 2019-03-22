cb82624b7f5b0b2a20fe7d6be38a4995be416b35
marlies ruck <marlies.ruck@gmail.com>
2019-03-21 21:56:09 -0700

    switching to mgo.v2 lib

--- frontend/database/mongo_test.go
+++ frontend/database/mongo_test.go
@@ -7,7 +7,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-type exception struct {
+type Exception struct {
 	Verb     string
 	Path     string
 	Class    string
@@ -15,39 +15,35 @@ type exception struct {
 	TargetID string
 }
 
-func (ex exception) ToBSON() bson.M {
+func (ex Exception) ToBSON() bson.M {
 	return bson.M{
 		"Verb": ex.Verb,
 		"Path": ex.Path,
 	}
 }
 
-func TestSanity(t *testing.T) {
-
-}
-
 func TestMongoClient(t *testing.T) {
-	//	t.Run("connect", func(t *testing.T) {
-	//		cli, err := NewClient("localhost", "27017", "athena")
-	//		require.NoError(t, err)
-	//		require.NotNil(t, cli)
-	//	})
-
-	t.Run("put then get", func(t *testing.T) {
+	t.Run("connect", func(t *testing.T) {
 		cli, err := NewClient("localhost", "27017", "athena")
 		require.NoError(t, err)
 		require.NotNil(t, cli)
-
-		//		exc := exception{Verb: "v", Path: "p"}
-		//		err = cli.WriteOne("exceptions", exc)
-		//		require.NoError(t, err)
-		//
-		//		result := exception{}
-		//		err = cli.ReadOne("exceptions", bson.M{"Verb": "v", "Path": "p"}, &result)
-		//		require.NoError(t, err)
-		//		require.Equal(t, "v", result.Verb)
-		//		require.Equal(t, "p", result.Path)
 	})
+
+	//	t.Run("put then get", func(t *testing.T) {
+	//		cli, err := NewClient("localhost", "27017", "athena")
+	//		require.NoError(t, err)
+	//		require.NotNil(t, cli)
+
+	//		exc := exception{Verb: "v", Path: "p"}
+	//		err = cli.WriteOne("exceptions", exc)
+	//		require.NoError(t, err)
+	//
+	//		result := exception{}
+	//		err = cli.ReadOne("exceptions", bson.M{"Verb": "v", "Path": "p"}, &result)
+	//		require.NoError(t, err)
+	//		require.Equal(t, "v", result.Verb)
+	//		require.Equal(t, "p", result.Path)
+	//	})
 	//	// read exception inserted from python
 	//	t.Run("read exception", func(t *testing.T) {
 	//		cli, err := NewClient("localhost", "27017", "athena")
