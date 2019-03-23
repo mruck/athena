3c01f5ce6c3c8137fae1ac2f67c17586f5e9ac27
marlies ruck <marlies.ruck@gmail.com>
2019-03-22 23:22:29 -0700

    add exceptions lib

---
+++ frontend/database/exceptions.go
@@ -0,0 +1,34 @@
+package database
+
+import "gopkg.in/mgo.v2"
+
+// Exception datatype
+type Exception struct {
+	Verb     string `bson:"Verb"`
+	Path     string `bson:"Path"`
+	Class    string `bson:"Class"`
+	Message  string `bson:"Message"`
+	TargetID string `bson:"TargetID"`
+}
+
+type ExceptionsManager struct {
+	collection *mgo.Collection
+}
+
+func NewExceptionsManager(db *mgo.Database) *ExceptionsManager {
+	return &ExceptionsManager{
+		collection: db.C("exceptions"),
+	}
+}
+
+func (manager *ExceptionsManager) GetAll(targetID string) ([]Exception, error) {
+	return nil, nil
+}
+
+func (manager *ExceptionsManager) ReadOne(filter interface{}, output interface{}) error {
+	return manager.collection.Find(filter).One(output)
+}
+
+func (manager *ExceptionsManager) WriteOne(exc Exception) error {
+	return manager.collection.Insert(exc)
+}
