c27dadc2b66da466166af7cb84ddb5e002d54ac4
marlies ruck <marlies.ruck@gmail.com>
2019-03-25 09:09:16 -0700

    sanity script for exceptions endpoint working

--- scripts/test_endpoints.sh
+++ scripts/test_endpoints.sh
@@ -3,16 +3,19 @@
 #      bash scripts/test_endpoints.sh
 
 set -e
+
+echo "Building and deploying frontend"
 #make frontend_deploy
+echo "Sleeping 120 while frontend deploys"
 #sleep 120
-## Upload target
+# Upload target
 containers=$(cat containers.json)
+echo "Hitting /FuzzTarget"
 pod=$(curl -d "$containers" http://35.192.59.73:30080/FuzzTarget)
 if [ -z “$pod” ]; then
     echo "Error spawning target"; echo $pod; exit
 fi
 echo "spawned pod: $pod"
-echo "Sleeping 60 before polling exceptions..."
-sleep 60
+sleep 120
 exceptions=$(curl http://35.192.59.73:30080/Exceptions/$pod)
 echo "exceptions: $exceptions"
