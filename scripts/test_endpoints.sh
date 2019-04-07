d2990123d6b9fca38ac17ad7643424422486ef73
marlies ruck <marlies.ruck@gmail.com>
2019-04-06 22:56:14 -0700

    Adding parsing of target id

--- scripts/test_endpoints.sh
+++ scripts/test_endpoints.sh
@@ -9,7 +9,10 @@ make frontend_deploy
 sleep 30
 containers=$(cat target_configs/discourse.json)
 echo "Hitting /FuzzTarget"
-target_id=$(curl -d "$containers" http://35.238.131.114:30080/FuzzTarget)
+target_meta=$(curl -d "$containers" http://35.238.131.114:30080/FuzzTarget | jq .skjdaskd)
+target_id=$(jq .TargetID <<< $target_meta)
+echo $target_id
+
 if [ -z “$target_id” ]; then
     echo "Error spawning target"; exit
 fi
