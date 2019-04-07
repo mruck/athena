7e9eb4474ea3e2f219a4fb5316e58d1ce0775584
marlies ruck <marlies.ruck@gmail.com>
2019-04-06 23:01:17 -0700

    captialize all vars for consistency and fix typo in script

--- scripts/test_endpoints.sh
+++ scripts/test_endpoints.sh
@@ -7,16 +7,17 @@ set -e
 echo "Building and deploying frontend"
 make frontend_deploy
 sleep 30
-containers=$(cat target_configs/discourse.json)
-echo "Hitting /FuzzTarget"
-target_meta=$(curl -d "$containers" http://35.238.131.114:30080/FuzzTarget | jq .skjdaskd)
-target_id=$(jq .TargetID <<< $target_meta)
-echo $target_id
 
-if [ -z “$target_id” ]; then
+echo "Hitting /FuzzTarget"
+CONTAINERS=$(cat target_configs/discourse.json)
+TARGET_META=$(curl -d "$CONTAINERS" http://35.238.131.114:30080/FuzzTarget)
+
+TARGET_ID=$(jq .TargetID <<< $TARGET_META)
+
+if [ -z “$TARGET_ID” ]; then
     echo "Error spawning target"; exit
 fi
-echo "spawned pod with target id: $target_id"
+echo "spawned pod with target id: $TARGET_ID"
 sleep 120
-exceptions=$(curl http://35.238.131.114:30080/Exceptions/$target_id)
-echo "exceptions: $exceptions"
+EXCEPTIONS=$(curl http://35.238.131.114:30080/Exceptions/$TARGET_ID)
+echo "EXCEPTIONS: $exceptions"
