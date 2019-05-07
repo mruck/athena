6b9db945e0046a850c72ecedee434ea0b408764e
marlies ruck <marlies.ruck@gmail.com>
2019-05-06 23:11:28 -0700

    moving discourse test pod to test folder

--- goFuzz/tests/test.sh
+++ goFuzz/tests/test.sh
@@ -4,7 +4,7 @@ set -xe
 
 kubectl delete pod discourse || true
 sleep 10
-kubectl apply -f ../../pods/discourse.test.pod.json
+kubectl apply -f discourse.test.pod.json
 sleep 60
 sudo pkill -9 kubectl
 kubectl port-forward discourse 8080:8080 > /dev/null 2>&1 &
