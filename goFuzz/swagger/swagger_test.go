30c04a51f2b4c7b87d4acd23fe08a689542f38a6
marlies ruck <marlies.ruck@gmail.com>
2019-05-09 21:34:14 -0700

    fixing swagger test

--- goFuzz/swagger/swagger_test.go
+++ goFuzz/swagger/swagger_test.go
@@ -3,10 +3,13 @@ package swagger
 import (
 	"testing"
 
-	"github.com/mruck/athena/goFuzz/util"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReadSwagger(t *testing.T) {
 	swagger := ReadSwagger("dummySwagger.json")
-	util.PrettyPrintStruct(swagger)
+	//util.PrettyPrintStruct(swagger)
+	// Check that a field is correct
+	description := swagger.Paths.Paths["/categories.json"].Post.Description
+	require.Equal(t, "Create a new category", description)
 }
