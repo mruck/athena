28143e246a55d0697ae725203861bcc06d6be703
marlies ruck <marlies.ruck@gmail.com>
2019-05-21 21:26:56 -0700

    Test how logging lib handles stack trace

---
+++ lib/log/log_test.go
@@ -0,0 +1,22 @@
+package log
+
+// Check how log lib prints stack trace.  Looks like we stil need to do
+// errors.WithStack
+
+import (
+	"fmt"
+	"testing"
+)
+
+func stack2() error {
+	//return errors.WithStack(fmt.Errorf("This is an error"))
+	return fmt.Errorf("This is an error")
+}
+
+func stack1() error {
+	return stack2()
+}
+
+func TestStackTrace(t *testing.T) {
+	Infof("%+v\n", stack1())
+}
