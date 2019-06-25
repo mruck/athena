b36a9f78475f35aad5d7448ba9b69380c218cd3a
marlies ruck <marlies.ruck@gmail.com>
2019-06-25 08:03:59 -0700

    adding some tests for mutation

--- goFuzz/mutator/naiveMutator_test.go
+++ goFuzz/mutator/naiveMutator_test.go
@@ -11,6 +11,7 @@ import (
 // PetStoreExpanded is path to pet store swagger with refs expanded (for testing)
 const PetStoreExpanded = "../tests/petstore_expanded.json"
 
+// Test mutating a path param
 func TestPathParam(t *testing.T) {
 	// Get the param obj
 	path := "/pet/{petId}"
@@ -26,8 +27,35 @@ func TestPathParam(t *testing.T) {
 	mutateParam(param)
 
 	util.PrettyPrintStruct(param, nil)
-	// Check we actually embedded something
-	//meta := ReadOneMetadata(param)
-	//require.Equal(t, []interface{}{}, meta.Values)
+	// TODO: Check we actually added a value with the same Not Nil check
+	// as below
+}
 
+// Test mutating body
+func TestBody(t *testing.T) {
+	// Get our param
+	path := "/store/order"
+	method := "post"
+	paramName := "body"
+	param, err := swagger.MockParam(PetStoreExpanded, path, method, paramName)
+	require.NoError(t, err)
+
+	// Embed a metadata obj
+	swagger.EmbedParam(param)
+
+	// Mutate the leaf nodes
+	mutateParam(param)
+
+	// Check that metadata.Values for each leaf node has
+	// valid data from our mutation
+	metadata := swagger.ReadAllMetadata(param)
+	for _, meta := range metadata {
+		require.NotNil(t, meta.Values)
+	}
+}
+
+func TestArrayWithPrimativeMetadata(t *testing.T) {
+}
+
+func TestMetaArrayWithObj(t *testing.T) {
 }
